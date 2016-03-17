16087947b56923dd28f884db73803fa2ff9c4b0c
brad-langshaw <brad.langshaw@live.ca>
2016-03-17 11:16:42 -0400

    made changes

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -3,8 +3,15 @@
 	"use strict";
 
 	var request = new XMLHttpRequest();
-    request.open('GET', '../people.txt', true);
+    request.open('GET', '../projects.json', true);
     request.addEventListener('readystatechange', function(){
+		var projects = {};
+		
+		projects = JSON.parse(request.responseText);
+		
+		var paragraphArray = [];
+		
+		paragraphArray = projects.paragraphs;
 		
 	});
 	request.send();	
