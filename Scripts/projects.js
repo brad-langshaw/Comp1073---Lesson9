7d6b726c15ce8b29e64e0c29772cd58fd667a084
brad-langshaw <brad.langshaw@live.ca>
2016-03-17 11:33:09 -0400

    injecting paragraphs

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -5,6 +5,7 @@
 	var request = new XMLHttpRequest();
     request.open('GET', '../projects.json', true);
     request.addEventListener('readystatechange', function(){
+	 if (request.readyState === 4) {
 		var projects = {};
 		
 		projects = JSON.parse(request.responseText);
@@ -13,6 +14,14 @@
 		
 		paragraphArray = projects.paragraphs;
 		
+		var paragraphArrayLength = paragraphArray.length;
+		
+		for(var number = 0; number < paragraphArrayLength; number++){
+		var paragraph = getElementbyid("paragraph" + (number+1) );
+		paragrapgh.innerHTML = paragraphArray[number];
+			
+		}
+	 }
 	});
 	request.send();	
 
