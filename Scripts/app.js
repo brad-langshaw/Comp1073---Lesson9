6a0204493518acc172c6824cb2239a5eec538750
brad-langshaw <brad.langshaw@live.ca>
2016-03-17 10:43:08 -0400

    update 1

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,52 +2,73 @@
 (function() {
 
     "use strict";
-    var output = "";
-    var firstParagraph = document.getElementById("firstParagraph");
+    
 
     // Instantiate new xhr object
     var request = new XMLHttpRequest();
-    request.open('GET', '../person.txt', true);
+    request.open('GET', '../people.txt', true);
     request.addEventListener('readystatechange', function() {
         if (request.readyState === 4) {
-			var addressBook = [];
-            var Person = {}; /* var Person = new Object();   */
+            var people = {};
+            people = JSON.parse(request.responseText);
 
-            addressBook = JSON.parse(request.responseText);
-			
-			console.log(addressBook)
-            
-            /* Person.sayHello = function() {
-                output += "<br><hr><br>" + Person.name + " says hello";
-            }
+            // declare our address array
+            var addressBook = []; /* var addressBook = new Array(); */
 
-            // for every key in the Person object, loop...
-            for (var key in Person) {
+            // read in the data from person.txt file
+            addressBook = people.addressBook;
 
-                // check if the key is the familyNames array
-                if (key === "familyNames") {
-                    output += "<br>Family Names: <br>";
-                    output += "<hr><br>";
-                    output += "<ul>";
-                    for (var index = 0; index < Person.familyNames.length; index++) {
-                        output += "<li>" + Person.familyNames[index] + "</li>";
-                    } // for loop
-                    output += "</ul>";
-                } // if statement
-                else if (key === "sayHello") {
-                    Person.sayHello();
+            var addressBookLength = addressBook.length;
+
+            // for each person in our addressbook....loop
+            for (var person = 0; person < addressBookLength; person++) {
+                // reset the string variable
+                var output = "";
+                // assign the sayHello method to each person object
+                addressBook[person].sayHello = function() {
+                    output += "<br><hr><br>" + addressBook[person].name + " says hello";
                 }
+
+                // for every key in the Person object, loop...
+                for (var key in addressBook[person]) {
+
+                    // check if the key is the familyNames array
+                    if (key === "familyNames") {
+                        output += "<br>Family Names: <br>";
+                        output += "<hr><br>";
+                        output += "<ul>";
+                        for (var index = 0; index < addressBook[person].familyNames.length; index++) {
+                            output += "<li>" + addressBook[person].familyNames[index] + "</li>";
+                        } // for loop
+                        output += "</ul>";
+                    } // if statement
+                    else if (key === "sayHello") {
+                        addressBook[person].sayHello();
+                    }
+
+                    // for all other cases do the following...
+                    else {
+
+                        output += addressBook[person][key] + "<br>";
+                    } // else statement
+
+                } // for in
                 
-                // for all other cases do the following...
-                else {
+                var paragraphString = "paragraph" + (person + 1);
+                console.log(paragraphString);
+                
+                var paragraph = document.getElementById(paragraphString)
+                paragraph.innerHTML = output;
 
-                    output += Person[key] + "<br>";
-                } // else statement
-					
-            } // for in
 
-            firstParagraph.innerHTML = output;*/
+            } // outer for loop
+
+
+
+
         }
+
+
     });
 
     request.send();
